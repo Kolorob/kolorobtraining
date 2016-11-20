dabdea9159745530fcd636a4bf41cb6400e4de18
liammagee <liam.magee@gmail.com>
2016-11-20 15:56:50 +0600

    Add anguler config

--- www/js/app.js
+++ www/js/app.js
@@ -79,6 +79,7 @@ angular.module('starter', ['ionic', 'nvd3', 'starter.controllers', 'starter.serv
     }
   });
 
+
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
 
